4961ec3b2db220b0a90691e6226a4faac4ffbc51
Arindam Paul <arindam017@gmail.com>
2018-12-28 01:34:29 +0600

    Update spatial-filters.js

--- assets/scripts/js/spatial-filters.js
+++ assets/scripts/js/spatial-filters.js
@@ -76,7 +76,7 @@ document.getElementById('laplacian').onclick = function() {
   let src = cv.imread('imageSrc');
   let dst = new cv.Mat();
   cv.cvtColor(src, src, cv.COLOR_RGBA2RGB, 0);
-  cv.Laplacian(src, dst, cv.CV_8U, 1, 1, 0, cv.BORDER_DEFAULT);
+  cv.Laplacian(src, dst, cv.CV_8U, 1, 0, 3, 1, 0, cv.BORDER_DEFAULT);
   cv.imshow('outputCanvas3', dst);
   src.delete();
   dst.delete();
@@ -92,7 +92,7 @@ document.getElementById('sobelX').onclick = function() {
   let dstx = new cv.Mat();
   //convert to binary
   cv.cvtColor(src, src, cv.COLOR_RGB2RGB, 0);
-  cv.Sobel(src, dstx, cv.CV_8U, 1, 1, 0, cv.BORDER_DEFAULT);
+  cv.Sobel(src, dstx, cv.CV_8U, 1, 0, 3, 1, 0, cv.BORDER_DEFAULT);
   cv.imshow('outputCanvas', dstx);
   src.delete();
   dstx.delete();
